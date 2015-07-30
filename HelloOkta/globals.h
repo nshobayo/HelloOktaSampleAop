bac8caf25684df8f9d2d4dbe1f049f804e92f40d
Nisola Shobayo <pshobayo@gmail.com>
2015-07-30 15:56:35 -0700

    path fix

---
+++ HelloOkta/globals.h
@@ -0,0 +1,15 @@
+//
+//  globals.h
+//  HelloOkta
+//
+//  Created by Nisola Shobayo on 6/26/15.
+//  Copyright (c) 2015 Nisola Shobayo. All rights reserved.
+//
+
+#ifndef HelloOkta_globals_h
+#define HelloOkta_globals_h
+#import "KeychainUtils.h"
+
+extern NSString *BASE_URL;
+extern KeychainUtils *keychainUtils;
+#endif
---
+++ HelloOkta/globals.m
@@ -0,0 +1,13 @@
+//
+//  globals.m
+//  HelloOkta
+//
+//  Created by Nisola Shobayo on 6/26/15.
+//  Copyright (c) 2015 Nisola Shobayo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KeychainUtils.h"
+
+NSString *BASE_URL = @"http://hello-okta.herokuapp.com";
+KeychainUtils *keychainUtils;
